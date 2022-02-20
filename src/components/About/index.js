5801aacc1adca3e609ed38eb0e296d31ee781c4f
Bryan A <bryanacostadelacruz@gmail.com>
2022-02-20 13:14:00 -0600

    react app init & basic components

---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+	return (
+		<section className="About" id="About">
+			<h1 className="text-primary">About</h1>
+		</section>
+	);
+}
+
+export default About;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="">
+						Home
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">
+						About
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">
+						Contact
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
